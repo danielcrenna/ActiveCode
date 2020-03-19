b86218456fb8201c8e5d7c097e49f893112db23c
Daniel Crenna <daniel.crenna@gmail.com>
2020-03-19 15:06:45 -0400

    port operation extensions

---
+++ src/ActiveErrors/ErrorEvents.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Daniel Crenna & Contributors. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace ActiveErrors
+{
+	public static class ErrorEvents
+	{
+		public const long AggregateErrors = 1004;
+		public const long ResourceMissing = 1006;
+	}
+}
