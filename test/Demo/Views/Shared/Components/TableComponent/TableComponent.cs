ca5c8180168f9a150ce611c92471b64634394865
Daniel Crenna <daniel.crenna@gmail.com>
2020-05-17 21:28:48 -0400

    demo table for logging objects

--- test/Demo/Views/Shared/Components/TableComponent/TableComponent.cs
+++ test/Demo/Views/Shared/Components/TableComponent/TableComponent.cs
@@ -1,22 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using TypeKitchen;
 
 namespace Demo.Views.Shared.Components.TableComponent
 {
-	public class TableModel
-	{
-		public IList<string> Headers { get; set; } = new List<string>();
-		public IList<TableRow> Rows { get; set; } = new List<TableRow>();
-	}
-
-	public class TableRow
-	{
-		public List<string> Cells { get; set; } = new List<string>();
-	}
-
 	public class TableComponent : ViewComponent
 	{
 		public TableComponent()
@@ -28,7 +16,9 @@ namespace Demo.Views.Shared.Components.TableComponent
 		{
 			var table = new TableModel();
 
-			var accessor = ReadAccessor.Create(models.First(), AccessorMemberTypes.Properties, AccessorMemberScope.Public, out var members);
+			var accessor = ReadAccessor.Create(models.First(), 
+				AccessorMemberTypes.Properties, 
+				AccessorMemberScope.Public, out var members);
 
 			foreach (var member in members)
 			{
